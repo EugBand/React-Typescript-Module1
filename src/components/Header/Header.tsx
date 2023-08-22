7b04255d6ca64eccbae3311c5719be78c3de8ec2
Yauhen Bandarenka <ybandarenka@ddy.systems>
2023-08-22 22:23:59 +0300

    Change domain from products to courses

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Logo from "./components/Logo/Logo";
+import {Button} from "../../common/Button";
+
 
 export const Header = () => (
     <nav className="h-[50px] flex justify-between px-5 bg-gray-500 items-center text-white">
+        <Logo />
+        <Button buttonClass={'bg-blue-400 border py-2 px-4'}>Login</Button>
         <span className="font-bold">EPAM TypeScript/React/Redux 2022</span>
-
         <span>
         <Link to="/" className="mr-2">Products</Link>
         <Link to="/about">About</Link>
---
+++ src/components/Header/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from "./fake-logo.png";
+
+const Logo: React.FC = () => (
+    <img src={logo} alt='Foake logo' width={64} height={32} />
+);
+
+export default Logo;
